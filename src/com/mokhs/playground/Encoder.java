29394d65f3cfcaf5335a6b843666dc8d5b90590c
mokhs00 <mokhs00@naver.com>
2021-07-15 22:36:35 +0900

    add : DI(Dependency Injection)

---
+++ src/com/mokhs/playground/Encoder.java
@@ -0,0 +1,15 @@
+package com.mokhs.playground;
+
+public class Encoder implements IEncoder{
+
+    private IEncoder iEncoder;
+
+    public Encoder(IEncoder iEncoder) {
+        this.iEncoder = iEncoder;
+    }
+
+    @Override
+    public String encode(String message) {
+        return iEncoder.encode(message);
+    }
+}
--- src/com/mokhs/playground/Main.java
+++ src/com/mokhs/playground/Main.java
@@ -3,6 +3,14 @@ package com.mokhs.playground;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        String message = "mokhs XD";
+
+        Base64Encoder base64Encoder = new Base64Encoder();
+        Encoder iBase64Encoder = new Encoder(base64Encoder);
+
+        String encode = iBase64Encoder.encode(message);
+        System.out.println(encode);
+
     }
 }
